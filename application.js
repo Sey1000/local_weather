f3708fc39b3978bb25e143d4d8e70b55bf6f97cb
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-10-01 22:54:09 +0200

    Use different api endpoint

--- application.js
+++ application.js
@@ -14,7 +14,9 @@ function processRequest(e) {
 }
 
 function requestWeather(coord) {
-  var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + coord[0] + "&lon=" + coord[1] + "&APPID=" + api_key;
+  var lon = Math.floor(coord[1]);
+  var lat = Math.floor(coord[0]);
+  var weatherUrl = "https://fcc-weather-api.glitch.me/api/current?lon=" + lon + "&lat=" + lat;
   var weatherXhr = new XMLHttpRequest();
   weatherXhr.open('GET', weatherUrl, true);
   weatherXhr.send()
@@ -23,15 +25,14 @@ function requestWeather(coord) {
   function processWeatherRequest(e) {
     if (weatherXhr.readyState == 4 && weatherXhr.status == 200) {
       var weatherResponse = JSON.parse(weatherXhr.responseText);
-      var temperature = Math.round((weatherResponse.main.temp - 273.15) * 10) / 10;
+      var temperature = Math.round((weatherResponse.main.temp) * 10) / 10;
       $('#temperature').text(temperature);
 
       var currentWeather = weatherResponse.weather[0].main;
       $('#weather').text(currentWeather);
 
-      var iconNum = weatherResponse.weather[0].icon;
-      var imgSrc = "http://openweathermap.org/img/w/" + iconNum + ".png";
-      $("#weather-icon").attr('src', imgSrc);
+      var icon = weatherResponse.weather[0].icon;
+      $("#weather-icon").attr('src', icon);
       readyForClick(temperature);
     }
   }
